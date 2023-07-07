7d33a1abb59e9c4984d9c89314f2192b39977d21
Tejas <tejaskothari03@gmail.com>
2023-07-08 01:38:03 +0530

    Destination class

---
+++ Destination.java
@@ -0,0 +1,22 @@
+import java.util.List;
+
+public class Destination {
+    private String name;
+    private List<Activity> activities;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Activity> getActivities() {
+        return activities;
+    }
+
+    public void setActivities(List<Activity> activities) {
+        this.activities = activities;
+    }
+}
