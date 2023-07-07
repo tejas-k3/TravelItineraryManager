601b2a60fc438f545f202ed53787ad2d87f802dc
Tejas <tejaskothari03@gmail.com>
2023-07-08 00:32:59 +0530

    Activity class

---
+++ Activity.java
@@ -0,0 +1,47 @@
+public class Activity {
+    private String name;
+    private String description;
+    private int price;
+    private int capacity;
+    private int currentCapacity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public void setCurrentCapacity(int currentCapacity) {
+        this.currentCapacity = currentCapacity;
+    }
+}
